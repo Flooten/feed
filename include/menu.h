54d5d47bb9e468d39f4a5b2d1f69d506f685c816
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2012-11-22 15:27:50 +0100

    Updated button size

--- include/menu.h
+++ include/menu.h
@@ -37,8 +37,8 @@ namespace feed
         std::vector<Button*> button_list_;
         glm::vec2 position_;
 
-        static const int X_OFFSET = 240;
-        static const int Y_OFFSET = 150;
+        static const int X_OFFSET = 30;
+        static const int Y_OFFSET = 70;
         static const int Y_SPACING = 20;
 
         glm::vec2 calculateButtonPosition(unsigned int index) const;
